d41e32c196afcbf7b97292e2be3c425e6e978437
Aditya Tiwari <Aditya Tiwari@Addy>
2018-04-15 12:49:09 +0530

    Upgrade with TestNG

--- src/runner/ExecuteTest.java
+++ src/runner/ExecuteTest.java
@@ -1,10 +1,12 @@
 package runner;
 
+import org.testng.annotations.Test;
+
 import script.Search;
 
 public class ExecuteTest {
-	
-	public static void main(String[] args) {
+	@Test
+	public void execute() {
 		Search s = new Search();
 		s.search();
 	}
